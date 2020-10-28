bafe0c449153cdb253fcd711164700877377c948
Rajat Handa <you@example.com>
2020-10-28 12:04:17 +0530

    first_save

--- website/forms.py
+++ website/forms.py
@@ -37,7 +37,7 @@ class signupform(forms.ModelForm):
         userobj.is_active = False
         if commit:
             userobj.save()
-            current_site = "localhost:8000"
+            current_site = "http://bestjalandharhotel.herokuapp.com/"
             email_subject = 'Activate Your Account'
             message = render_to_string('activate_account.html', {
                 'user': userobj,
